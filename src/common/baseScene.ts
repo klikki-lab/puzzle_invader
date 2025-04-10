974b585026b42cc02340aa294f62cec8a596880e
klikki-lab <klikki.lab@gmail.com>
2025-04-10 17:12:10 +0900

    スクリーンショット用の処理を追加

--- src/common/baseScene.ts
+++ src/common/baseScene.ts
@@ -8,4 +8,17 @@ export abstract class BaseScene<T> extends g.Scene {
     public get onFinish(): ((param: T) => void | undefined) { return this._onFinish; }
 
     public set onFinish(onFinish: (param: T) => void) { this._onFinish = onFinish; }
+
+    /**
+    * 右クリックでスクリーンショットできるようにする。この処理を有効にするにはtsconfigにdomを追加すること。
+    */
+    enableRightClickScreenshot = (): void => {
+        this.onPointDownCapture.add(ev => {
+            if (ev.button === 2) {
+                const link = document.getElementsByClassName("pure-button")[2];
+                (link as HTMLAnchorElement).click();
+                return;
+            }
+        });
+    };
 }
