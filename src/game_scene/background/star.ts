347a1bdb60cc1708d0418958afe5e4738a889cbd
klikki-lab <klikki.lab@gmail.com>
2025-04-12 08:06:36 +0900

    ワープを修正

--- src/game_scene/background/star.ts
+++ src/game_scene/background/star.ts
@@ -4,7 +4,7 @@ export class Star extends g.FilledRect {
     private static readonly OFFSET_Y = g.game.height * 0.1;
     private static readonly WIDTH = g.game.width * 0.8;
     private static readonly HEIGHT = g.game.height * 0.8;
-    private static readonly MAX_VELOCITY_Y = g.game.height / 2;
+    private static readonly MAX_VELOCITY_Y = g.game.height / 32;
     private static readonly SIZE = 2;
 
     private vy = 0;
@@ -33,19 +33,23 @@ export class Star extends g.FilledRect {
 
     private updateHandler = (): void | boolean => {
         if (this.isWarp) {
-            const rate = (1 / g.game.fps) * 64;
-            this.vy += rate;
-            this.height += rate * 2;
-            if (this.vy > Star.MAX_VELOCITY_Y) {
-                this.vy = Star.MAX_VELOCITY_Y;
-                console.log(this.vy)
+            const size = this.width * this.width;
+            const maxVelocity = Star.MAX_VELOCITY_Y * size
+            if (this.vy < maxVelocity) {
+                const rate = (1 / g.game.fps) * 16 * size;
+                this.vy += rate;
+                this.height += rate;
+                if (this.vy > maxVelocity) {
+                    this.vy = maxVelocity;
+                    this.height = maxVelocity;
+                }
             }
         } else {
             if (this.isWarping()) {
                 this.vy *= 0.8;
                 this.height *= 0.8;
-                if (this.vy < this.width / 2) {
-                    this.vy < this.width / 2;
+                if (this.height < this.width) {
+                    this.vy = this.width / 2;
                     this.height = this.width;
                 }
             }
@@ -57,7 +61,7 @@ export class Star extends g.FilledRect {
         this.modified();
     };
 
-    private isWarping = (): boolean => this.vy > this.width / 2;
+    private isWarping = (): boolean => this.height > this.width;
 
     private init = (): void => {
         if (!this.isWarping()) {
